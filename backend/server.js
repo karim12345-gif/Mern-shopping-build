2bb8d7ede9e9953be947f1898ab18c52aed2809f
karim tariq salim <karim991996@gmail.com>
2021-08-04 15:35:56 +0400

    deployment3

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,13 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.json({ message: "API running..." });
-// });
+
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   })
 } else {
   app.get('/', (req, res) => {
